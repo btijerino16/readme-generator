1d59d8a5cb427f44b5c16056bc26a0c1e576ed33
btijerino16 <92263027+btijerino16@users.noreply.github.com>
2022-01-09 12:29:43 -0700

    shouldnt be nodemodules

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 
-// TODO: Include packages needed for this application
 // TODO: Create an array of questions for user input
 const questions = [
 
